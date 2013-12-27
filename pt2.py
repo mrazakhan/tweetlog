0153de8b4ef7032bd894e4f5e2043b4eb69987da
razaurrehman <razaurrehman@gmail.com>
2013-12-26 23:50:28 -0800

    Working Code with location and key word tracking

--- pt2.py
+++ pt2.py
@@ -9,23 +9,31 @@ import csv
 import twitter
 from twitter.oauth import write_token_file, read_token_file
 from twitter.oauth_dance import oauth_dance
-
+import json
 import datetime
 import dateutil.tz
 from sys import argv
+from pymongo import Connection
 
 consumer_key="P8joRY0TqzliQOijOeAZvQ"
 consumer_secret="XWdgCqhh77jA9U2orKsLJBNK8C8h4qwYesiOi61goM"
 access_token="2246602044-bst3A05437YMnVTMvbpMwx5ztNZFlViYznUp5EU"
-access_secret="PptUTWU320Cif1yMhkrN7gUhYGcohvq0V4I3BeVajFavH111"
+access_secret="PptUTWU320Cif1yMhkrN7gUhYGcohvq0V4I3BeVajFavH"
 token_file = '../out/twitter.oauth'
 token_path ='../out/'
 out_path='../dump/'
 app_name = 'SeattleLog'
 
-debug=1
-files_per_tweet=10
+debug=0
+files_per_tweet=100
 words = 'seattle'
+
+latlng = ["47.5,-122.5,48.5,-121.5","47.5,-122.5,48.5,-121.5"]
+connection = Connection()
+
+db = connection.test
+
+
 """
 Login method requires to enter a pin code only once then it stores it in a file that can be used again and again
 """
@@ -65,17 +73,25 @@ def dump_tweets(log_file=0):
 	ts = get_twitter_stream()
 
 
-	res = ts.statuses.filter(track=words)
+	res = ts.statuses.filter(track=words, location=latlng)
 	
 	for r in res:
 		if (len(tweetlist)<files_per_tweet):
 			print len(tweetlist),
 			tweetlist.append(r)
+			if debug==1:
+				print r
+			'''fout = open('temp.txt','w')
+			encoded_data = json.dumps(r)
+			print 'decoded_data:' , encoded_data
+			fout.write(encoded_data)
+			fout.close()
+			sys.exit(0)'''
 			tweetfieldset = tweetfieldset.union(r.keys())
 
-			if debug==1:
-				if r[u'text']:
-					print (r[u'text'])
+			
+			if r[u'text']:
+				print (r[u'text'])
 		else:
 			break
 	
@@ -91,20 +107,30 @@ def dump_tweets(log_file=0):
 		fname = out_path+datetime_now+'-'+tzn+'.json'
 
 		f = open(fname, 'w')
-
+		for r2 in tweetlist:
+			encoded_data = json.dumps(r2)
+			f.write(encoded_data+'\n')
 		#dw = csv.DictWriter(f, fieldnames = list(tweetfieldset))
 
 		#dw.writeheader()
-
+		'''
 		for r2 in tweetlist:
-		#	dw.writerow({k:v.encode('utf8') if isinstance(v,unicode) else v for k,v in r2.items()}) 
+			#dw.writerow({k:v.encode('utf8') if isinstance(v,unicode) else v for k,v in r2.items()}) 
 			items=[]
+
+			db.tweets.save(r2)
+
+			with open('tmp.txt','w') as outfile:
+				json.dump(r2, outfile)
+			f.write('{')
 			for k, v in r2.items():
 				val =v.encode('utf8') if isinstance(v,unicode) else v
 				items.append('{0}:{1}'.format(k,val))
-			f.write(','.join(items)+'\n')		
+			f.write(','.join(items))
+			f.write('}\n')
+		'''		
 		f.close()
-
+		#sys.exit(0)
 
 
 
--- pt2_mongo.py
+++ pt2_mongo.py
@@ -10,9 +10,12 @@ Additionally, for each user we want to download the information
 about his friends and followers as well
 
 '''
-
+from sys import argv, exit
 import pymongo
+
+import json
 import os
+from bson import json_util
 connection = pymongo.Connection("localhost", 27017)
 tweets_db = connection.tweets
 debug=1
@@ -23,19 +26,32 @@ def LoadToMongo(input_dir,output_dir):
 
 	files=[]
 	for (dirpath, dirnames, filenames) in os.walk(input_dir):
-		f.extend(filenames)
+		files.extend(filenames)
 		break
-	
+	if not os.path.isdir(output_dir):
+		os.mkdir(output_dir)	
 	for f in files:
-		
+		print ' Processing file %s \n' %(f)
+		for r in open(input_dir+f).readlines():
+			
+			tweets_db.tweets_table.insert(json.loads(r))
 	
-	
+		os.rename(input_dir+f, output_dir+f)
 if __name__=='__main__':
 	
+	if len(argv)!=3:
+		print ' Invalid number of args'
+		print ' pt2_mongo.py input_dir output_dir'
+		exit(-1)
 	prog_name, input_dir,output_dir=argv
 	
+	if not input_dir.endswith('/'):
+		input_dir+='/'
+
+	if not output_dir.endswith('/'):
+		output_dir+='/'
 	if debug==1:
-		print ' %s called , input_dir: %s, output_dir: %s' %(prog_name, input_dirm output_dir)
+		print ' %s called , input_dir: %s, output_dir: %s' %(prog_name, input_dir,output_dir)
 
 	LoadToMongo(input_dir, output_dir)	
 	
